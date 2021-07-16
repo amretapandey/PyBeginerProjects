510cc636841eb504a65cce141d00377ad385c0db
Amrita Pandey <69110282+amretapandey@users.noreply.github.com>
2021-07-16 22:06:50 +0530

    Create Run-Length Encoding.py

---
+++ Run-Length Encoding.py
@@ -0,0 +1,17 @@
+def runLengthEncoding(string):
+    encodedString = []
+    currentRunLength = 1 
+    for i in range(1, len(string)):
+        if string[i] != string[i - 1] or currentRunLength == 9:
+            encodedString.append(str(currentRunLength))
+            encodedString.append(string[i - 1])
+            currentRunLength = 0
+        currentRunLength += 1
+    encodedString.append(str(currentRunLength))
+    encodedString.append(string[-1])
+    return ''.join(encodedString)
+    
+
+string = input()
+print(runLengthEncoding(string))
+
